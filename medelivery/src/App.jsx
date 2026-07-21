3bb3e84b81ec569e1b13ea6324aab72532b0205b
Gowri Menon <menongowri16@gmail.com>
2026-07-21 18:54:44 +0530

    added the router-dom to link pages

--- medelivery/src/App.jsx
+++ medelivery/src/App.jsx
@@ -1,26 +1,37 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import SearchBar from "./components/SearchBar";
-import Pharmacies from "./pages/pharmacies";
 import ProductList from "./components/ProductList";
+
+import Cart from "./pages/cart";
+import Pharmacies from "./pages/pharmacies";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
-
-function App() {
-
-    return(
-
+function Home() {
+    return (
         <>
-            <Navbar />
             <Hero />
             <SearchBar />
-            <pharmacies />
             <ProductList />
-
         </>
-
     );
+}
 
+function App() {
+    return (
+        <BrowserRouter>
+            <Navbar />
+
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/pharmacies" element={<Pharmacies />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
--- medelivery/src/components/card.jsx
+++ medelivery/src/components/card.jsx
@@ -2,6 +2,8 @@ function Card()
 {
     return(
         <div className="card">
+            <h1>Medicine</h1>
+            <img></>
         </div>
     );
 }
